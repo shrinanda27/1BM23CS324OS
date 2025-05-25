c7d3f415969d21ba717a76ac3655e8a7b4666a7e
Shrinanda Dinde <shrinanda.cs23@bmsce.ac.in>
2025-05-25 12:00:32 +0530

    Update FIFO.c

--- pagereplacement/FIFO.c
+++ pagereplacement/FIFO.c
@@ -1,35 +1,43 @@
 #include <stdio.h>
 
 int main() {
-    int n, frames, i, j, k, found, index = 0, page_faults = 0, hits = 0;
-    char pages[100];
+	int frames, pages, pageFaults = 0, current = 0;
+	int referenceString[100], frame[100]; // Max size to prevent runtime issues
 
-    printf("Enter the size of the pages:\n");
-    scanf("%d", &n);
-    printf("Enter the page strings:\n");
-    scanf("%s", pages);
-    printf("Enter the no of page frames:\n");
-    scanf("%d", &frames);
-
-    int mem[frames];
-    for (i = 0; i < frames; i++) mem[i] = -1;
-
-    for (i = 0; i < n; i++) {
-        found = 0;
-        for (j = 0; j < frames; j++) {
-            if (mem[j] == pages[i] - '0') {
-                hits++;
-                found = 1;
-                break;
-            }
-        }
-        if (!found) {
-            mem[index] = pages[i] - '0';
-            index = (index + 1) % frames;
-            page_faults++;
-        }
+	printf("Enter number of frames: "); 
+    scanf("%d", &frames); 
+    printf("Enter number of pages: "); 
+    scanf("%d", &pages); 
+    printf("Enter reference string: "); 
+    for (int i = 0; i < pages; i++) { 
+        scanf("%d", &referenceString[i]); 
     }
 
-    printf("FIFO Page Faults: %d, Page Hits: %d\n", page_faults, hits);
-    return 0;
+	// Initialize frames
+	for (int i = 0; i < frames; i++) {
+		frame[i] = -1;
+	}
+
+	for (int i = 0; i < pages; i++) {
+		int found = 0;
+		for (int j = 0; j < frames; j++) {
+			if (frame[j] == referenceString[i]) {
+				found = 1;
+				break;
+			}
+		}
+		if (!found) {
+			frame[current] = referenceString[i];
+			current = (current + 1) % frames;
+			pageFaults++;
+		}
+		printf("%d: ", referenceString[i]); 
+        for (int k = 0; k < frames; k++) { 
+        printf("%d ", frame[k]); 
+        } 
+        printf("\n");
+	}
+
+	printf("Total Page Faults: %d\n", pageFaults);
+	return 0;
 }
