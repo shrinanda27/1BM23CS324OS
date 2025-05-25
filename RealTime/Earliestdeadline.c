97fc9d2b42a7e8dd21d5e9e6ed7a56adcf249e68
Shrinanda Dinde <shrinanda.cs23@bmsce.ac.in>
2025-05-25 20:08:28 +0530

    Update Earliestdeadline.c

--- RealTime/Earliestdeadline.c
+++ RealTime/Earliestdeadline.c
@@ -1,79 +1,84 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int gcd(int a, int b) {
-    while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
+#define MAX_TASKS 10
+
+typedef struct {
+    int id;
+    int CT;             // Computation Time
+    int RT;             // Remaining Time
+    int period;         // Period (release interval)
+    int next_release;   // Next release time
+    int deadline;       // Relative deadline
+    int abs_deadline;   // Absolute deadline (calculated)
+} Task;
+
+// Sort tasks by earliest absolute deadline
+void sort_by_deadline(Task tasks[], int n) {
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < n - i; j++) {
+            if (tasks[j].abs_deadline > tasks[j + 1].abs_deadline || 
+                (tasks[j].abs_deadline == tasks[j + 1].abs_deadline && tasks[j].id > tasks[j + 1].id)) {
+                Task temp = tasks[j];
+                tasks[j] = tasks[j + 1];
+                tasks[j + 1] = temp;
+            }
+        }
     }
-    return a;
 }
 
-int lcm(int a, int b) {
-    return (a * b) / gcd(a, b);
-}
-
-struct Process {
-    int id, burst_time, deadline, period;
-};
-
-void earliest_deadline_first(struct Process p[], int n, int time_limit) {
-    int time = 0;
-    printf("Earliest Deadline Scheduling:\n");
-    printf("PID\tBurst\tDeadline\tPeriod\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d\t%d\t\t%d\t\t%d\n", p[i].id, p[i].burst_time, p[i].deadline, p[i].period);
-    }
-
-    printf("\nScheduling occurs for %d ms\n", time_limit);
-    while (time < time_limit) {
-        int earliest = -1;
+void EDF(Task tasks[], int n, int totaltime) {
+    for (int t = 0; t < totaltime; t++) {
+        // Release new instances of tasks if needed
         for (int i = 0; i < n; i++) {
-            if (p[i].burst_time > 0) {
-                if (earliest == -1 || p[i].deadline < p[earliest].deadline) {
-                    earliest = i;
-                }
+            if (t == tasks[i].next_release) {
+                tasks[i].RT = tasks[i].CT;
+                tasks[i].abs_deadline = t + tasks[i].deadline;
+                tasks[i].next_release += tasks[i].period;
+                printf("Time %d: Task %d released (Deadline at %d)\n", t, tasks[i].id, tasks[i].abs_deadline);
             }
         }
 
-        if (earliest == -1) break;
+        // Sort tasks by deadline
+        sort_by_deadline(tasks, n);
 
-        printf("%dms: Task %d is running.\n", time, p[earliest].id);
-        p[earliest].burst_time--;
-        time++;
+        // Execute task with earliest deadline
+        int executed = 0;
+        for (int i = 0; i < n; i++) {
+            if (tasks[i].RT > 0 && t < tasks[i].abs_deadline) {
+                printf("Time %d: Executing Task %d\n", t, tasks[i].id);
+                tasks[i].RT--;
+                executed = 1;
+
+                if (tasks[i].RT == 0) {
+                    printf("Time %d: Task %d completed", t + 1, tasks[i].id);
+                    if (t + 1 > tasks[i].abs_deadline) {
+                        printf(" (Deadline Missed!)");
+                    }
+                    printf("\n");
+                }
+                break;
+            }
+        }
+
+        if (!executed) {
+            printf("Time %d: CPU Idle\n", t);
+        }
     }
 }
 
 int main() {
-    int n;
-    printf("Enter the number of processes: ");
-    scanf("%d", &n);
+    // Define periodic tasks: {id, CT, RT, period, next_release, deadline, abs_deadline}
+    Task tasks[MAX_TASKS] = {
+        {1, 2, 0, 5, 0, 5, 0},  // Task 1: CT=2, Period=5, Deadline=5
+        {2, 1, 0, 3, 0, 3, 0},  // Task 2: CT=1, Period=3, Deadline=3
+        {3, 1, 0, 7, 0, 7, 0}   // Task 3: CT=1, Period=7, Deadline=7
+    };
 
-    struct Process processes[n];
-    printf("Enter the CPU burst times:\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &processes[i].burst_time);
-        processes[i].id = i + 1;
-    }
+    int numTasks = 3;
+    int totalTime = 20;
 
-    printf("Enter the deadlines:\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &processes[i].deadline);
-    }
-
-    printf("Enter the time periods:\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &processes[i].period);
-    }
-
-    int hyperperiod = processes[0].period;
-    for (int i = 1; i < n; i++) {
-        hyperperiod = lcm(hyperperiod, processes[i].period);
-    }
-
-    printf("\nSystem will execute for hyperperiod (LCM of periods): %d ms\n", hyperperiod);
-
-    earliest_deadline_first(processes, n, hyperperiod);
+    EDF(tasks, numTasks, totalTime);
 
     return 0;
 }
