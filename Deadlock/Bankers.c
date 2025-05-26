768c6782721d30ba1afebb9aa7f59f68c3ee6e79
Shrinanda Dinde <shrinanda.cs23@bmsce.ac.in>
2025-05-26 12:45:59 +0530

    Update Bankers.c

--- Deadlock/Bankers.c
+++ Deadlock/Bankers.c
@@ -1,78 +1,58 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 int main() {
-    int n, m, i, j, k;
-    printf("Enter number of processes: ");
-    scanf("%d", &n);
-    printf("Enter number of resources: ");
-    scanf("%d", &m);
+	int n, m, i, j, k;
+	printf("Enter the number of processes and resources : ");
+	scanf("%d %d", &n, &m);
 
-    int alloc[n][m], max[n][m], avail[m];
-    int need[n][m];
+	int allo[n][m], request[n][m], avail[m];
+	printf("Enter the allo matrix:\n");
+	for (i = 0; i < n; i++)
+		for (j = 0; j < m; j++)
+			scanf("%d", &allo[i][j]);
 
-    printf("Enter allocation matrix (%d x %d):\n", n, m);
-    for (i = 0; i < n; i++) {
-        printf("Allocation for process %d: ", i);
-        for (j = 0; j < m; j++)
-            scanf("%d", &alloc[i][j]);
-    }
+	printf("Enter the request matrix:\n");
+	for (i = 0; i < n; i++)
+		for (j = 0; j < m; j++)
+			scanf("%d", &request[i][j]);
 
-    printf("Enter max matrix (%d x %d):\n", n, m);
-    for (i = 0; i < n; i++) {
-        printf("Max for process %d: ", i);
-        for (j = 0; j < m; j++)
-            scanf("%d", &max[i][j]);
-    }
+	printf("Enter the avail resources:\n");
+	for (i = 0; i < m; i++)
+		scanf("%d", &avail[i]);
 
-    printf("Enter available resources (%d values): ", m);
-    for (i = 0; i < m; i++)
-        scanf("%d", &avail[i]);
+	int finished[n], deadlocked[n], deadlock_count = 0;
+	for (i = 0; i < n; i++) finished[i] = 0;
 
-    for (i = 0; i < n; i++)
-        for (j = 0; j < m; j++)
-            need[i][j] = max[i][j] - alloc[i][j];
+	while (1) {
+		int found = 0;
+		for (i = 0; i < n; i++) {
+			if (!finished[i]) {
+				for (j = 0; j < m; j++)
+					if (request[i][j] > avail[j]) break;
+				if (j == m) {
+					for (k = 0; k < m; k++)
+						avail[k] += allo[i][k];
+					finished[i] = 1;
+					found = 1;
+				}
+			}
+		}
+		if (!found) break;
+	}
 
-    bool finish[n];
-    int safeSeq[n];
-    int count = 0;
+	for (i = 0; i < n; i++) {
+		if (!finished[i]) {
+			deadlocked[deadlock_count++] = i;
+		}
+	}
 
-    for (i = 0; i < n; i++)
-        finish[i] = false;
-
-    while (count < n) {
-        bool found = false;
-        for (i = 0; i < n; i++) {
-            if (!finish[i]) {
-                for (j = 0; j < m; j++)
-                    if (need[i][j] > avail[j])
-                        break;
-
-                if (j == m) {
-                    for (k = 0; k < m; k++)
-                        avail[k] += alloc[i][k];
-
-                    safeSeq[count++] = i;
-                    finish[i] = true;
-                    found = true;
-                }
-            }
-        }
-
-        if (!found) {
-            printf("System is not in safe state.\n");
-            return 1;
-        }
-    }
-
-    printf("System is in safe state.\n");
-    printf("Safe sequence is: ");
-    for (i = 0; i < n; i++) {
-        printf("P%d", safeSeq[i]);
-        if (i != n - 1)
-            printf(" -> ");
-    }
-    printf("\n");
-
-    return 0;
+	if (deadlock_count == 0)
+		printf("No deadlock detected.\n");
+	else {
+		printf("Deadlock detected. Deadlocked processes: ");
+		for (i = 0; i < deadlock_count; i++)
+			printf("%d ", deadlocked[i]);
+		printf("\n");
+	}
+	return 0;
 }
